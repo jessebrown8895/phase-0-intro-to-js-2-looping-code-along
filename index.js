cebf30e0166dc494b42aaf005c5b513d294a10e8
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-29 16:38:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+function writeCards(names){
+    for(let i = 0; i < names.length; i++){
+        console.log(names[i])
+        let newArray = `Thank you, ${names[i]}, for the suprise`
+        return newArray
+    }
+}
+
