b2044fb0f6b3a295fb854a0af1edef6fa67d0764
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-29 16:39:26 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function writeCards(names){
     for(let i = 0; i < names.length; i++){
         console.log(names[i])
         let newArray = `Thank you, ${names[i]}, for the suprise`
-        return newArray
+        
     }
+    return newArray
 }
 
